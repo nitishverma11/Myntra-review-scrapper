cad75a75dc27693858a8039bb1ea7d3e450dd21e
nitishverma11 <nitishv944@gmail.com>
2024-11-26 10:38:56 +0530

    First commit

---
+++ README.md
@@ -0,0 +1,1 @@
+## "Myntra Review scrapper "
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages ,setup
+
+setup(
+    name="myntra scrapper",
+    version = "0.0.1",
+    author= "Nitish",
+    author_email= "nitishv944@gmail.com",
+   packages= find_packages(),
+    install_requires = []
+)
